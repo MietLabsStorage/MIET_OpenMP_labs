ddeb96ce34880b3683bfd6c286dc6153861a4223
m_a_ks_ <61201152+mJaJksJ@users.noreply.github.com>
2020-09-10 14:19:19 +0300

    done

---
+++ Lab1Var1.h
@@ -0,0 +1,4 @@
+#pragma once
+int countSevensInNum(int num);
+void runLab1v1Lin(int** matrix, int N, int M);
+void runLab1v1Par(int** matrix, int N, int M);
---
+++ Matrix.h
@@ -0,0 +1,3 @@
+#pragma once
+void matrixInit(int** matrix, int N, int M);
+void matrixShow(int** matrix, int N, int M);
